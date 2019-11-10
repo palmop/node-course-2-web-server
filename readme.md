057ec2cec8891b81f1a630d00de86d84c7317680
palmonari <paolo.palmonari@monrif.net>
2019-11-10 18:01:39 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# command line 
+- `npm install express --save`  
+- `sud npm install nodemon -g`  
+- `nomemon server.js`  
+- `hpm install hbs --save`
+
+# links
+- (express)[https://expressjs.com]
+- (handlebars)[https://www.handlebarsjs.com], (npm hbs)[https://www.npmjs.com/package/hbs]
+
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+// directory views is the default for hbs
+app.set('view engine', hbs);
+
+hbs.registerHelper('list', function(items, options) {
+    //console.log(items);
+    //console.log(options);
+    var out = "<ul>";
+    for(var i=0, l=items.length; i<l; i++) {
+      //out = out + "<li>" + items[i]['name'] + "</li>";
+      // nell' hbs c'è {{#list likes}}{{name}}{{/list}}
+      // che vuole dire che "fn" chiede la proprietà 'name' dell'iesimo likes
+      out = out + "<li>" + options.fn(items[i]) + "</li>";
+    }
+    return out + "</ul>";
+});
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+    if (text) return text.toUpperCase();
+    return "";
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.path}`;
+    console.log(log);
+    fs.appendFile('server.log', log + "\n", (err) => {
+        if (err) console.log("unable to open server.log");
+    });
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('mantainance.hbs');
+// })
+
+app.get('/', (req, res) => {
+    res.render(
+        'home.hbs',
+        {
+            pageTitle: 'home page',
+            Name: "palmonaz",
+            welcome: "welcome to zimbawie",
+            likes: [
+                {name:'bike'},
+                {name:'electric skate'},
+                {name:'capoeira'}
+            ]
+        });
+});
+
+app.get('/about', (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        code: 3004,
+        message: 'generic error'
+    })
+});
+
+app.listen(3000, () => {
+    console.log("server started and listen on 3000");
+});
